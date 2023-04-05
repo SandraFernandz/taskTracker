9bf41f0a90d6aa6e3d353688f855b8f20fa08e6e
Sandra Fernandz <sandraenmunich@gmail.com>
2023-04-05 11:27:59 +0200

    added components to App

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,5 +1,17 @@
+import ListForm from './components/ListForm';
+import TaskList from './components/TaskList';
+import TaskSearch from './components/TaskSearch';
+import TaskDuration from './components/TaskDuration';
+
 const App = () => {
-  return <div>hi!</div>;
+  return (
+    <div>
+      <ListForm />
+      <TaskList />
+      <TaskSearch />
+      <TaskDuration />
+    </div>
+  );
 };
 
 export default App;
