43b3eec641c45c683f863b5cf3efe03fe528ef56
Sandra Fernandz <sandraenmunich@gmail.com>
2023-04-06 10:06:57 +0200

    created reducers

---
+++ task-tracker/src/store/slices/formSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from 'react-redux';
+
+const formSlice = createSlice({
+  name: 'form',
+  initialState: {
+    name: '',
+    duration: 0,
+  },
+  reducers: {
+    changeName(state, action) {
+      state.name = action.payload;
+    },
+    changeDuration(state, action) {
+      state.cost = action.payload;
+    },
+  },
+});
+
+export const { changeName, changeDuration } = formSlice.actions;
+export const formReducer = formSlice.reducer;
---
+++ task-tracker/src/store/slices/tasksSlice.js
@@ -0,0 +1,34 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const tasksSlice = createSlice({
+  name: 'tasks',
+  initialState: {
+    searchTask: '',
+    cars: [],
+  },
+  reducers: {
+    changeSearchTask(state, action) {
+      state.searchTask = action.payload;
+    },
+    addTask(state, action) {
+      // Assumption:
+      // action.payload === { name: 'ab', cost: 140}
+      state.tasks.push({
+        name: action.payload.name,
+        duration: action.payload.duration,
+        id: nanoid(),
+      });
+    },
+    removeTask(state, action) {
+      // Assumption:
+      // action.payload === id of the car we want to remove
+      const updated = state.cars.filter((car) => {
+        return car.id !== action.payload;
+      });
+      state.cars = updated;
+    },
+  },
+});
+
+export const { changeSearchTask, addTask, reducers } = tasksSlice.actions;
+export const tasksReducer = tasksSlice.reducer;
