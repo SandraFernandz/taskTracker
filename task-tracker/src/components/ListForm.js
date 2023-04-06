9306676384db914513bdae62718f3cda2bea59bd
Sandra Fernandz <sandraenmunich@gmail.com>
2023-04-06 17:56:14 +0200

    added input

--- task-tracker/src/components/ListForm.js
+++ task-tracker/src/components/ListForm.js
@@ -1,5 +1,27 @@
-const ListForm = () => {
-  return <div>ListForm</div>;
+import { useDispatch, useSelector } from 'react-redux';
+import { changeName } from '../store';
+
+const TaskForm = () => {
+  const dispatch = useDispatch();
+  const name = useSelector((state) => {
+    return state.form.name;
+  });
+  const handleNameChange = (event) => {
+    dispatch(changeName(event.target.value));
+  };
+  return (
+    <div>
+      <h4>Add Task</h4>
+      <form>
+        <div>
+          <div>
+            <label>Name</label>
+            <input value={name} onChange={handleNameChange}></input>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
-export default ListForm;
+export default TaskForm;
