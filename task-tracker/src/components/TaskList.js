13f956661c545911a2a88811530cc9e1421d6d8f
Sandra Fernandz <sandraenmunich@gmail.com>
2023-04-07 08:47:00 +0200

    added handle function and task list render

--- task-tracker/src/components/TaskList.js
+++ task-tracker/src/components/TaskList.js
@@ -1,11 +1,25 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeTask } from '../store/slices/tasksSlice';
 
 const TaskList = () => {
+  const dispatch = useDispatch();
   const tasks = useSelector((state) => {
     return state.tasks.tasksData;
   });
-  console.log(tasks);
-  return <div>TaskForm</div>;
+  const handleTaskDelete = (task) => {
+    dispatch(removeTask(task.id));
+  };
+  const renderedTasks = tasks.map((task) => {
+    return (
+      <div key={task.id}>
+        <p>
+          {task.name} {task.duration} Minutes
+        </p>
+        <button onClick={() => handleTaskDelete(task)}>Delete</button>
+      </div>
+    );
+  });
+  return <div>{renderedTasks}</div>;
 };
 
 export default TaskList;
