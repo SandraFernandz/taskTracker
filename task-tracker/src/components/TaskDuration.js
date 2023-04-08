08c31b728cdc7577e9828cea57ab48dc18334247
Sandra Fernandz <sandraenmunich@gmail.com>
2023-04-08 14:15:35 +0200

    refactored with reducer

--- task-tracker/src/components/TaskDuration.js
+++ task-tracker/src/components/TaskDuration.js
@@ -5,11 +5,15 @@ const TaskDuration = () => {
     const filteredTasks = tasksData.filter((task) => {
       return task.name.toLowerCase().includes(searchTask.toLowerCase());
     });
-    let duration = 0;
-    for (let task of filteredTasks) {
-      duration += task.duration;
-    }
-    return duration;
+    // let duration = 0;
+    // for (let task of filteredTasks) {
+    //  duration += task.duration;
+    // }
+    //return duration;
+    // can also be calculated with a reducer:
+    return filteredTasks.reduce((acc, task) => {
+      return acc + task.duration;
+    }, 0);
   });
   return <div>Total Minutes: {totalMinutes}</div>;
 };
