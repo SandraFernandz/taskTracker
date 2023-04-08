a8210a81ea822d9ddaaa7f46bd0bf52e1830862a
Sandra Fernandz <sandraenmunich@gmail.com>
2023-04-08 14:12:11 +0200

    created function to calculate total duration

--- task-tracker/src/components/TaskDuration.js
+++ task-tracker/src/components/TaskDuration.js
@@ -1,5 +1,17 @@
+import { useSelector } from 'react-redux';
+
 const TaskDuration = () => {
-  return <div>TaskDuration</div>;
+  const totalMinutes = useSelector(({ tasks: { tasksData, searchTask } }) => {
+    const filteredTasks = tasksData.filter((task) => {
+      return task.name.toLowerCase().includes(searchTask.toLowerCase());
+    });
+    let duration = 0;
+    for (let task of filteredTasks) {
+      duration += task.duration;
+    }
+    return duration;
+  });
+  return <div>Total Minutes: {totalMinutes}</div>;
 };
 
 export default TaskDuration;
